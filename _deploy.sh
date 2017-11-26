1750ffe37128fcff4bbf5085669a471594d90901
w201rdada-portfolio-daveowenbrg-aniqla4c <dave.owen@berkeley.edu>
2017-11-26 21:17:02 +0000

    deploy

--- _deploy.sh
+++ _deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 # put your info here and uncomment so git (and GitHub) knows who your commits belong to!
-# git config user.email "calnet@berkeley.edu"
-# git config user.name "Oski Bear"
+git config user.email "dave.owen@berkeley.edu"
+git config user.name "DaveOwenBRG"
 
 # make sure you're on the master branch
 git checkout master
